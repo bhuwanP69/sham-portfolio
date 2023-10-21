bd276dd4c3e45d5f120fb25f70fcb7c796f96c52
Bhuwan <bhubanpaudel79@gmail.com>
2023-10-21 18:18:18 +0545

    made button color and animation

--- src/pages/First.jsx
+++ src/pages/First.jsx
@@ -1,7 +1,19 @@
 export default function First() {
     return (
       <div className="first h-screen pt-24 bg-body-color" id="first">
-       first
+       <h1>Hi, i'm Shamar</h1>
+       <h5>I build web and mobile apps.</h5>
+       <div className="buttons flex">
+        <button className="flex items-center  text-white gap-2 bg-gradient-to-r from-button-color1  to-button-color2 px-5 py-3 rounded-full ">
+          <p>My Projects</p>
+          <i class="fa-solid fa-chevron-right text-sm pt-1 left-to-right-bounce "></i>
+        </button>
+        <button className="flex items-center  text-white gap-2 bg-gradient-to-r from-button-color1  to-button-color2 px-5 py-3 rounded-full ">
+          <p>My Projects</p>
+          <i class="fa-regular fa-comment text-sm pt-1  "></i>
+        </button>
+       
+       </div>
       </div>
     )
   }
