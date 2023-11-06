af38442fd8dd42e9a69b4fb5cd7024fe20488cce
Bhuwan <bhubanpaudel79@gmail.com>
2023-11-06 14:10:55 +0545

    made button white cover

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -17,6 +17,14 @@ export default function RootLayout() {
  
   window.addEventListener('scroll', handleScroll);
   const logo = './src/images/logo.png'
+ 
+
+  const handleDownload = () => {
+    const pdfUrl = './src/images/Shamar_Morrison_Web_Developer_Resume.pdf';
+     // Open a new tab/window to display the PDF.
+     window.open(pdfUrl);
+  };
+
   return (
     <div className=''>
       <header className= {`root-layout  fixed w-full flex pl-20  bg-body-color justify-between items-center pr-28 transition-all duration-500 z-20   ${scrolled ?'bg-white h-20':'bg-body-color  h-24'}`}>
@@ -40,7 +48,15 @@ export default function RootLayout() {
           <span className=" absolute block left-0 right-0 bottom-0  h-[0.75px]  bg-aboutText scale-x-0 transform  transition-transform ease-in-out  origin-right duration-300  group-hover:scale-x-105 group-hover:origin-left "></span>
            
           </Link>
-          <Link classID=""><button className=" bg-gradient-to-r from-button-color1  to-button-color2 text-white px-6 py-3  text-base  font-sans rounded-3xl">Download CV</button></Link>
+          <a 
+          ><button onClick={handleDownload} className=" relative bg-gradient-to-r from-button-color1  to-button-color2 text-white px-6 py-3  text-base  font-sans rounded-3xl overflow-hidden group">Download CV
+
+          <span class="absolute -top-2 -right-2 transform rotate-45 rounded-full opacity-20 h-24 w-12 bg-white group-hover:w-24 transition-all duration-1000 overflow-hidden"></span>
+{/* 
+          <span class="absolute top-4 rotate-6 -right-5 rounded-full opacity-20 h-8 w-1/2 bg-white hover:w-32 transition-all duration-1000"></span> */}
+
+
+          </button></a>
         </nav>
       </header>
     
--- src/pages/First.jsx
+++ src/pages/First.jsx
@@ -1,18 +1,29 @@
+import { Link } from "react-scroll";
+
 export default function First() {
     return (
       <div className="first h-screen text-center pt-48 bg-body-color" id="first">
        <h1 className="text-[95px] font-extrabold font-sans text-transparent bg-clip-text bg-gradient-to-r from-text-color from-40% via-text-color via-50%   to-text-color2 to-1%">Hi, i'm Shamar</h1>
        <h5 className="text-3xl font-medium text-text-Black pb-14">I build web and mobile apps.</h5>
        <div className="buttons flex gap-5 justify-center">
-        <button className="flex items-center  text-white gap-2 bg-gradient-to-r from-button-color1  to-button-color2 px-8 py-4 rounded-full ">
+        <Link to="projects" smooth>
+        <button className="flex items-center  text-white gap-2 bg-gradient-to-r from-button-color1  to-button-color2 px-8 py-4 rounded-full  group relative overflow-hidden">
+
+     
+        <span class="absolute -top-2 -right-2 transform rotate-45 rounded-full opacity-20 h-24 w-14 bg-white group-hover:w-28 group-hover:rotate-12 transition-all duration-1000 overflow-hidden"></span>
+        
           <p className="text-lg">My Projects</p>
           <i class="fa-solid fa-chevron-right text-sm pt-1 left-to-right-bounce "></i>
         </button>
-        <button className="flex items-center  text-white gap-2 bg-gradient-to-r from-button-color2  to-text-color2 px-8 py-4  rounded-full ">
+        </Link>
+        <Link to="contact" smooth>
+        <button className="flex items-center  text-white gap-2 bg-gradient-to-r from-button-color2  to-text-color2 px-8 py-4  rounded-full relative group overflow-hidden ">
+        <span class="absolute -top-2 -right-5 transform rotate-45 rounded-full opacity-20 h-24 w-14 bg-white group-hover:w-28 group-hover:rotate-12 transition-all duration-1000 overflow-hidden"></span>
 
           <p className="text-lg">Let's Talk</p>
-          <i class="fa-solid fa-comment text-sm pt-1  "></i>
+          <i class="fa-solid fa-comment text-sm  "></i>
         </button>
+        </Link>
        
        </div>
       </div>
