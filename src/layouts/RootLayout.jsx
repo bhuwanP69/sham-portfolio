2de99f9a6bcacc75b06aedca6312ee15e8ec2866
Bhuwan <bhubanpaudel79@gmail.com>
2023-11-10 14:07:18 +0545

    made sidebar

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Outlet, NavLink } from "react-router-dom";
 import { Link } from "react-scroll";
 
@@ -24,17 +24,84 @@ export default function RootLayout() {
      // Open a new tab/window to display the PDF.
      window.open(pdfUrl);
   };
+  
+  const [showNav, setShowNav] = useState(false);
+   // Ref to the more-info container
+   const moreInfoRef = useRef(null);  
+   
+  function handleMoreClick() {
+    setShowNav(!showNav);
+  }
+ 
+  
+    // Function to handle clicks outside 
+    const handleClickOutside = (event) => {
+      const button = document.querySelector('.fa-bars'); 
+      if (button && button.contains(event.target)) {
+        return;
+      }
+
+      if (moreInfoRef.current && !moreInfoRef.current.contains(event.target)) {
+          setShowNav(false);
+      }
+  };
+
+  // Attach click event listener to detect clicks outside the more-info section
+  useEffect(() => {
+      document.addEventListener('mousedown', handleClickOutside);
+      return () => {
+          document.removeEventListener('mousedown', handleClickOutside);
+      };
+  }, []);
 
   return (
     <div className=''>
-      <header className= {`root-layout  fixed w-full flex pl-20  bg-body-color justify-between items-center pr-28 transition-all duration-500 z-20   ${scrolled ?'bg-white h-20':'bg-body-color  h-24'}`}>
+      <header className= {`root-layout  fixed w-full flex pl-20   bg-body-color justify-between items-center pr-28 transition-all duration-1000 z-20   ${scrolled ?'bg-white h-20 py-8':'bg-body-color  h-24 py-0'}`}>
         <Link smooth to="first" spy={true} duration={500} className="cursor-pointer w-28">
         <img src={logo} alt="logo"  />
         </Link>
-        <button className="block lg:hidden">
-        <i className="fa-solid fa-bars   text-2xl cursor-pointer"></i>
+        <button onClick={handleMoreClick} className={`block lg:hidden z-30  absolute   right-10  `}>
+        <i className={`fa-solid fa-bars   text-2xl  w-14 cursor-pointer px-2 py-1 rounded-lg ${showNav ? 'border-black border-4 pointer-events-none':' border-none'}}`}></i>
         </button>
-        <nav className=" lg:flex justify-between font-semibold text-gray-800  w-[500px] hidden ">
+
+        {/* sidebar  */}
+        {showNav &&
+        <nav ref={moreInfoRef} className={`sidebar flex flex-col gap-5 w-48 h-full bg-black text-white fixed  top-0 left-0 opacity-100 transition-all duration-500  `} >
+          {/* home  */}
+          
+        <Link  smooth to="home" spy={true} duration={500} className="cursor-pointer py-6  bg-white text-black text-lg   text-center  ">Home
+           
+          </Link>
+        <Link  smooth to="about" spy={true} duration={500} className=" relative cursor-pointer  text-lg text-center  pb-5">About
+            {/* underline  */}
+            <span className="absolute left-0 right-0 bottom-0  h-[0.75px]  bg-aboutText scale-x-100  opacity-50"></span>
+          </Link>
+          
+          <Link  smooth to="projects" spy={true} duration={500} className="cursor-pointer    relative   text-lg   text-center  pb-5">Projects
+           {/* underline  */}
+           <span className="absolute left-0 right-0 bottom-0  h-[0.75px]  bg-aboutText scale-x-100 opacity-50 "></span>
+          
+           
+          </Link>
+          <Link  smooth to="contact" spy={true} duration={500} className="cursor-pointer   relative text-lg text-center  pb-5">Contact
+          {/* underline  */}
+          <span className="absolute left-0 right-0 bottom-0  h-[0.75px]  bg-aboutText scale-x-100 opacity-50 "></span>
+          </Link>
+          <a  className="relative"
+          ><button onClick={handleDownload} className=" text-text-color cursor-pointer  text-lg pl-8  pb-5 ">Download CV
+               <span className="absolute left-0 right-0 bottom-0  h-[0.75px]  bg-aboutText scale-x-100 opacity-50 "></span>
+          </button></a>
+        </nav>
+        }
+        {showNav && (
+        <div
+          className={`fixed inset-0 left-48 bg-black cursor-pointer z-10 opacity-50  ${scrolled ?'top-20':'top-0'} `}
+        />
+      )}
+
+        {/* big screen  */}
+        
+        <nav className={` lg:flex hidden justify-between font-semibold text-gray-800  w-[500px]  z-40 `}>
         
           
           <Link  smooth to="about" spy={true} duration={500} className="cursor-pointer pt-3 hover:opacity-80 group relative ">About
@@ -62,6 +129,7 @@ export default function RootLayout() {
 
           </button></a>
         </nav>
+     
       </header>
     
       <main>
